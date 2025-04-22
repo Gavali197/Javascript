b70ad7c61c44f107bf37820592ae4c40a35cebaa
Gavali197 <161225822+Gavali197@users.noreply.github.com>
2025-04-22 21:32:16 +0530

    complete 4 quetion today

---
+++ ifelseque.js
@@ -0,0 +1,75 @@
+/*1. If-Else (Basic)
+Q: Write a program that checks if a number is even or odd.
+
+let num = prompt("enter Number check number are odd or even");
+if(num % 2 === 0){
+    console.log("number are even");
+}else{
+    console.log("number are odd");
+}
+
+*/
+
+
+
+/*Create a program that compares two numbers and prints which one is larger.
+
+let a = prompt("enter frist number")
+let b = prompt("enter second number")
+if (a>b) {
+    console.log("a is greter");
+}else{
+    console.log("b is greter");
+}
+*/
+
+
+// Create a traffic light system that suggests actions ("Go", "Slow down", "Stop") based on light color.
+
+// let light = prompt("Enter Light Color")
+// if(light === "green"){
+//     console.log("Go");
+// }
+// if(light === "red"){
+//     console.log("Stop");
+// }
+// if(light === "yellow"){
+//     console.log("Slow down");
+// }
+    
+
+/*Loop (For Loop)
+Q: Print numbers from 1 to 10 using a for loop.*/
+
+// let a = 1;
+// for(a; a<=10; a++){
+//     console.log(a);
+// }
+
+
+/*3. String (Length Check)
+Q: Check if a string's length is greater than 5.*/
+
+// let a = prompt("enter string");
+// if (a.length >= 5){
+//     console.log("length are more then 5");
+// }else{
+//     console.log("lenght are small then 5");
+// }
+
+
+
+// 4. Array Method (Push & Pop)
+// Q: Add an element to an array and then remove the last element.
+
+
+// a = ["hemant", "rahul", "jiya"]
+// a.push("priya");
+// a.pop("hemant")
+// console.log(a);
+
+
+
+// 5. Loop (While Loop)
+// Q: Print numbers from 10 to 1 in reverse using a while loop.
+
